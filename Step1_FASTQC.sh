06d14898a8d15d0e16b43d2910e47469054cf35b
lwaymire <waymireliz@gmail.com>
2024-10-30 13:34:15 -0500

    Update Step1_FASTQC.sh

--- Step1_FASTQC.sh
+++ Step1_FASTQC.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/bash
 #PBS -l nodes=1:ppn=5
  
-# locating the directory with data
-cd /data/
+# Here we are locating the directory with data that we are working on and the results will be saved.
+# "<absolute/path/to/the/working/directory>" part to the absoulute path to your data files
+cd <absolute/path/to/the/working/directory>
  
 # setting background for conda environment activation
 eval "$(conda shell.bash hook)"
